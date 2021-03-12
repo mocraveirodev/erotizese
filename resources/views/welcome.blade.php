c566f3c262d2148fa30f003da27d7338f84fdbb2
Monica Craveiro <monica.craveiro@mondodx.com>
2021-03-12 01:20:32 -0300

    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="description" content="Erotize-se Sex Shop">
+        <meta name="keywords" content="erotize-se, sex shop">
+        <meta name="author" content="Monica Craveiro">
+
+        <title>EROTIZE-SE - Sex Shop Online</title>
+        
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
+        <!-- Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+        <!-- Font Awesome -->
+        <script src="https://kit.fontawesome.com/24828d29ae.js" crossorigin="anonymous"></script>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+        <header>
+            <div class="top-banner">
+                <!-- <img class="banner-img" src="assets/img/top-banner.png" alt="Banner Topo Erotize-se"> -->
+                <div class="title d-flex flex-column align-items-center p-2">
+                    <h1>Eroti<br>ze-se!</h1>
+                    <h2 class="align-self-end">Sex Shop</h2>
+                </div>
+            </div>
+            <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #1D1D1D;">
+                <div class="container flex-row justify-content-around">
+                    <a class="navbar-brand" href="#"><img class="img-fluid" width="30" src="assets/img/favcon1.jpg" alt="Ícone Erotize-se"></a>
+                    <ul class="navbar-nav w-50 flex-row justify-content-around">
+                        <li class="nav-item">
+                            <a class="nav-link" href="http://www.facebook.com/erotize.ss"><i class="fab fa-facebook"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://www.instagram.com/erotize.ss/"><i class="fab fa-instagram"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://wa.me/message/2BZ5UYKUOJOWO1"><i class="fab fa-whatsapp"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="mailto:erotizese.sexshop@gamil.com"><i class="far fa-envelope"></i></a>
+                        </li>
+                    </ul>
+                    <a class="navbar-brand" href="#"><img class="img-fluid" width="30" src="assets/img/favcon1.jpg" alt="Ícone Erotize-se"></a>
+                </div>
+            </nav>
+        </header>
+        <main>
+            <section id="products-menu">
+                <div class="buttons">
+                    <button class="btn-hover color-3">MASTURBADORES</button>
+                    <button class="btn-hover color-7">POMPOARISMO</button>
+                    <button class="btn-hover color-9">FANTASIAS</button>
+                    <button class="btn-hover color-1">VIBRADORES</button>
+                    <button class="btn-hover color-11">DIVERSÃO</button>
+                    <button class="btn-hover color-10">LUBRIFICANTES</button>
+                    <button class="btn-hover color-4">KITS</button>
+                    <!-- <button class="btn-hover color-2">LUBRIFICANTES</button>
+                    <button class="btn-hover color-5">DIVERSÃO</button>
+                    <button class="btn-hover color-6">button</button>
+                    <button class="btn-hover color-8">BUTTON</button> -->
+                </div>
+            </section>
+            <section id="buy-safe">
+                <div class="container">
+                    <img class="img-fluid" src="assets/img/compra segura.png" alt="Compra segura">
+                </div>
+            </section>
+        </main>
+
+        <!-- Bootstrap JS -->
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+    </body>
+</html>
